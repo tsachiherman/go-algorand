62fd826f842d6692b7c645cf67bfd8aea0de54d4
Tsachi Herman <tsachi.herman@algorand.com>
2020-11-24 16:29:57 -0500

    changes

--- cmd/algokey/generate.go
+++ cmd/algokey/generate.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -33,6 +34,58 @@ func init() {
 	generateCmd.Flags().StringVarP(&generatePubkeyfile, "pubkeyfile", "p", "", "Public key filename")
 }
 
+// https://stackoverflow.com/a/50285590/356849
+func toUint11Array(arr []byte) []uint32 {
+	var buffer uint32
+	var numberOfBit uint32
+	var output []uint32
+
+	for i := 0; i < len(arr); i++ {
+		// prepend bits to buffer
+		buffer |= uint32(arr[i]) << numberOfBit
+		numberOfBit += 8
+
+		// if there enough bits, extract 11bit number
+		if numberOfBit >= 11 {
+			// 0x7FF is 2047, the max 11 bit number
+			output = append(output, buffer&0x7ff)
+
+			// drop chunk from buffer
+			buffer = buffer >> 11
+			numberOfBit -= 11
+		}
+
+	}
+
+	if numberOfBit != 0 {
+		output = append(output, buffer&0x7ff)
+	}
+	return output
+}
+
+func toByteArray(arr []uint32) []byte {
+	var buffer uint32
+	var numberOfBits uint32
+	var output []byte
+
+	for i := 0; i < len(arr); i++ {
+		buffer |= uint32(arr[i]) << numberOfBits
+		numberOfBits += 11
+
+		for numberOfBits >= 8 {
+			output = append(output, byte(buffer&0xff))
+			buffer >>= 8
+			numberOfBits -= 8
+		}
+	}
+
+	if numberOfBits != 0 {
+		output = append(output, byte(buffer))
+	}
+
+	return output
+}
+
 var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generate key",
@@ -41,7 +94,23 @@ var generateCmd = &cobra.Command{
 		var seed crypto.Seed
 		crypto.RandBytes(seed[:])
 
-		mnemonic := computeMnemonic(seed)
+		i := 0
+		var mnemonic string
+		for {
+			mnemonic = computeMnemonic(seed)
+			if len(strings.Split(mnemonic, " ")[i]) != 4 {
+
+				uintArray := toUint11Array(seed[:])
+				uintArray[i] = uint32(crypto.RandUint64() % 2048)
+
+				copy(seed[:], toByteArray(uintArray))
+				continue
+			}
+			i++
+			if i > 23 {
+				break
+			}
+		}
 
 		key := crypto.GenerateSignatureSecrets(seed)
 		publicKeyChecksummed := basics.Address(key.SignatureVerifier).String()
